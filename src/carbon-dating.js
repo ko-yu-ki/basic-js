1fa1c77b7af490c74aac9610b69f82cf36e650f0
ko-yu-ki <55734445+ko-yu-ki@users.noreply.github.com>
2020-09-29 13:52:00 +0300

    feat: carbon-dating solution, count-cats solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,12 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (!sampleActivity || typeof(sampleActivity) !== 'string' || +sampleActivity !== 'number') return false;
+
+  let k = Math.LN2/HALF_LIFE_PERIOD;
+  let t = 0;
+  t = Math.ceil((Math.log(15/+sampleActivity))/k);
+  return t;
 };
+
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(catsMatrix) {
+  if (!catsMatrix || typeof(catsMatrix) !== 'object') return false;
+  let cats = 0;
+  for (let i = 0; i < catsMatrix.length; i++) {
+    for (let j = 0; j < catsMatrix[i].length; j++) {
+      if (catsMatrix[i][j] === '^^') {cats++;}
+    }
+  }
+  console.log(cats);
+  return cats;
 };
