59af8a64099b6ad5bd1f2a76e8a2c06db1d79295
ko-yu-ki <55734445+ko-yu-ki@users.noreply.github.com>
2020-09-29 16:19:11 +0300

    feat: add solution to dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+    if (typeof(members) !== 'object' || !members) {return false;}
+    let commandName='';
+    let i = 0;
+    let member ='';
+    while (i < members.length) {
+      if (typeof(members[i]) === 'string') {
+        member = members[i].trim();
+        commandName += member[0].toUpperCase();
+      }
+      i++;
+    }
+  
+  return commandName.split('').sort().join('');
+  };
